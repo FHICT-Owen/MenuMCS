50def94642d9a076e7c578bb9e679a1fd1e34440
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-09-21 12:19:36 +0200

    Added JPA, GetCategories

---
+++ menu-service/src/main/java/com/digitalmenu/menuservice/category/CategoryConfig.java
@@ -0,0 +1,20 @@
+package com.digitalmenu.menuservice.category;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CategoryConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(CategoryRepository repository) {
+        return args -> {
+            new Category(
+                    1L,
+                    "Vlees"
+            );
+        };
+    }
+
+}

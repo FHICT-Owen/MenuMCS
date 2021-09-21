e3ce4a7be7dd14679e536e837a20d37abc91c658
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-09-21 12:33:49 +0200

    Added Hibernate

--- menu-service/src/main/java/com/digitalmenu/menuservice/category/Category.java
+++ menu-service/src/main/java/com/digitalmenu/menuservice/category/Category.java
@@ -2,12 +2,12 @@ package com.digitalmenu.menuservice.category;
 public class Category {
     private long Id;
     private String Name;
-    public Category() {
-    }
+
     public Category(long id, String name) {
         Id = id;
         Name = name;
     }
+
     public long getId() {
         return Id;
     }

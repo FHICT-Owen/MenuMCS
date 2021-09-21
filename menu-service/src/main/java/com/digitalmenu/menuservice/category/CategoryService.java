91c2048810e48126ab74e423d8cc32c066f62bbb
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-09-21 12:13:06 +0200

    created delete without database call
    Calls delete method, when database is setup the code for deleting should be added

--- menu-service/src/main/java/com/digitalmenu/menuservice/category/CategoryService.java
+++ menu-service/src/main/java/com/digitalmenu/menuservice/category/CategoryService.java
@@ -1,4 +1,17 @@
 package com.digitalmenu.menuservice.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+
+    public CategoryService()
+    {
+    }
+
+    public void DeleteCategory(Long categoryId)
+    {
+        System.out.println("category " + categoryId + " deleted!");
+    }
 }

a89ae0efdaa00d016c0e615135da4aa67ffd34fc
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-09-27 13:25:42 +0200

    ree

--- src/main/java/com/digitalmenu/menuservice/dish/DishController.java
+++ src/main/java/com/digitalmenu/menuservice/dish/DishController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("api/v1/dish")
